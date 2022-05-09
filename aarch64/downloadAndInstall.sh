d04c5d02e0cfb27b36d5deda5dbad0ef4114a4c2
bkmartinjr <bruce@chanzuckerberg.com>
2022-05-09 16:36:31 -0700

    fix bug in aarch64 build

--- aarch64/downloadAndInstall.sh
+++ aarch64/downloadAndInstall.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+function version_gt() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" != "$1"; }
+
 TAGS=$(curl -s https://api.github.com/repos/chanzuckerberg/shasta/releases | grep -oP '"tag_name": "\K(.*)(?=")')
 echo "Found release tags: $TAGS".
 for TAG in $TAGS
 do
-    if [[ "$TAG" > "0.5.1" ]]; then
-   	EXECUTABLE="shasta-Linux-ARM-$TAG"
+    if version_gt "$TAG" "0.5.1"; then
+   		EXECUTABLE="shasta-Linux-ARM-$TAG"
     	curl -O -L https://github.com/chanzuckerberg/shasta/releases/download/$TAG/$EXECUTABLE
     	chmod ugo+x $EXECUTABLE
     	echo "Done downloading $EXECUTABLE."    
